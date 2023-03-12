e6e1c3f6849a94d74cee7cdb817787f69fe1ad2b
Edward Tanguay (windows) <edward@tanguay.info>
2023-03-12 14:05:44 +0100

    import works

--- dev/createdb.mjs
+++ dev/createdb.mjs
@@ -1,5 +1,8 @@
 import * as tools from './tools.mjs';
 
 const connectionUrl = 'mongodb://localhost:27017';
-const dbName = 'starter-simple-mongo-006';
-tools.createDB(connectionUrl, dbName, 'jobs', 'skills');
+const dbName = 'starter-simple-mongo-009';
+tools.createDB(connectionUrl, dbName, ['jobs', 'skills']);
+
+tools.importDataIntoCollection(connectionUrl, dbName, 'jobs', './dev/data/jobs.json');
+tools.importDataIntoCollection(connectionUrl, dbName, 'skills', './dev/data/skills.json');
--- dev/tools.mjs
+++ dev/tools.mjs
@@ -1,4 +1,5 @@
 import { MongoClient } from 'mongodb';
+import fs from "fs";
 
 /**
  * Function to create a MongoDB database and collections
@@ -6,17 +7,22 @@ import { MongoClient } from 'mongodb';
  * @param {string} databaseName
  * @param {string[]} collectionNames 
  */
-export const createDB = (connectionString, databaseName, ...collectionNames) => {
+export const createDB = (connectionString, databaseName, collectionNames) => {
 	MongoClient.connect(connectionString, (err, client) => {
 		if (err) {
 			console.log(`ERROR: ${err.message}`);
 		} else {
 			const dbo = client.db(databaseName);
 			let collectionsCreated = 0;
+			let count = 0;
 			collectionNames.forEach(collectionName => {
+				count++;
 				dbo.createCollection(collectionName, (err) => {
 					if (err) {
 						console.log(`ERROR: ${err.message}`);
+						if (count === collectionNames.length) {
+							client.close();
+						}
 					} else {
 						collectionsCreated++;
 						if (collectionsCreated === collectionNames.length) {
@@ -28,3 +34,29 @@ export const createDB = (connectionString, databaseName, ...collectionNames) => 
 		}
 	});
 };
+
+/**
+ * Imports data into MongoDB collection
+ */
+export const importDataIntoCollection = (connectionString,dataBaseName, collectionName, file) => {
+    MongoClient.connect(connectionString, (err, db) => {
+        if (err) throw err;
+        const dbo = db.db(dataBaseName)
+
+        fs.readFile(file, (err, data) => {
+            if (err) throw err;
+            const documents = JSON.parse(data);
+
+            documents.forEach((document) => {
+                delete document._id
+                delete document.__v
+
+            })
+
+            dbo.collection(collectionName).insertMany(documents, (err, res) => {
+                if (err) throw err;
+                db.close();
+            });
+        })
+    })
+}
